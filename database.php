90a44f5c52a2015e3834976a568a3f924b990a02
maksimsoul <39761095+maksimsoul@users.noreply.github.com>
2019-01-21 18:14:29 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect('localhost', 'root', '', 'maksimisgreat')
+    $link = mysqli_connect('localhost', 'root', '', 'articles')
     or die(mysqli_error($link));
 
     mysqli_set_charset($link, "utf8") or die("Не установлена кодировка соединения");

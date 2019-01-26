6281c9ad124c14666eb9568be524229dab674eae
maksimsoul <maksimisgreat@gmail.com>
2019-01-26 14:20:43 +0200

    New commit

--- views/articles.php
+++ views/articles.php
@@ -5,45 +5,78 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="shortcut icon" href="images/php_logo.jpg" type="image/jpg">
+
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <title>PHP и MySQL</title>
 </head>
 
 <body>
-
 <header>
-    <div id="headphp" col-lg-6 col-lg-offset-0>
-        Статьи о PHP и MySQL
-    </div>
+    <div class="container">
 
-    <div class="head" col-lg-6>
-        <img src="images/php-00441489844a0103eae71ba2fe15244c.png" alt="PHP + MySQL" class="imgphp">
+        <div class="row">
+
+            <div class="col-lg-5 col-lg-offset-0">
+                <a href="index.php"><img src="images/php.png" alt="PHP + MySQL" id="imgphp"></a>
+            </div>
+
+            <div id="headphp" class="col-lg-7 align-self-center">
+                Статьи о PHP и MySQL
+            </div>
+
+        </div>
+
     </div>
 </header>
 
-<nav>
-    <ul>
-        <li><a href="">Главная</a></li>
-        <li><a href="">Переменные</a></li>
-        <li><a href="">Функции</a></li>
-        <li><a href="">Циклы</a></li>
-        <li><a href="">ООП</a></li>
-    </ul>
-</nav>
+<div id="navphp">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark container">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Главная<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Переменные</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Функции</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Циклы</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ООП</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+
+    </nav>
+</div>
+
 <div class="container">
     <?php foreach ($articles as $article): ?>
-        <div>
-            <h2><a href="controllers/article.php?id=<?=$article['id']?>">Тема статьи: <?=$article['title']?></a></h2>
-            <h3>Рубрика: <?=$article['category']?></h3>
-            <em>Опубликовано: <?=$article['date']?></em>
-            <p><?=$article['text']?></p>
+        <div class="articles rounded">
+            <div class="row article">
+                <h2 class="col-lg-8"><a href="controllers/article.php?id=<?=$article['id']?>"><?=$article['title']?></a></h2>
+                <h3 class="col-lg-4">Рубрика: <?=$article['category']?></h3>
+            </div>
+            <p class="font-italic date">Опубликовано: <?=$article['date']?></p>
+            <p class="art_text"><?=$article['text']?></p>
         </div>
     <?php endforeach ?>
 </div>
 <footer>
     <p>
-        PHP - programming language
+        Copyright © 2019 The PHP-site Group
     </p>
 </footer>
 </body>
